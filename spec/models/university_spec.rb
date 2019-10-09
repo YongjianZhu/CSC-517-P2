ec5419b54afea893f9dcba5e366836043c7d2710
Qingyan Wang <346829766@qq.com>
2019-10-09 01:02:31 -0400

    rspec model tested

--- spec/models/university_spec.rb
+++ spec/models/university_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe University, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   it "is valid with valid attributes" do
-    expect(University.new).to be_valid
+    expect(University.new(name: "NCSU")).to be_valid
+  end
+
+  it "is valid with valid attributes" do
+    expect(University.new(name: "")).to_not be_valid
   end
 
 end
